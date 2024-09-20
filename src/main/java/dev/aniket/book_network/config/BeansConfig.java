f0c114aff985d3ca8e38ddce024f16425b6aba87
Aniket205kadam <aniketrkadam205@gmail.com>
2024-09-20 16:32:24 +0530

    auditor aware: Auditing

--- src/main/java/dev/aniket/book_network/config/BeansConfig.java
+++ src/main/java/dev/aniket/book_network/config/BeansConfig.java
@@ -3,6 +3,7 @@ package dev.aniket.book_network.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -22,6 +23,11 @@ public class BeansConfig {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
--- src/main/java/dev/aniket/book_network/feedBack/FeedBack.java
+++ src/main/java/dev/aniket/book_network/feedBack/FeedBack.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Arrays;
+
 @Getter
 @Setter
 @SuperBuilder
